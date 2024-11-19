3344a7ed718c943f6fa68796dd8b2f9d6f3446f8
Yared Weldegebrial <yared12707@gmail.com>
2024-11-18 21:49:25 -0600

    test for automatic deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello world from Netlify!</div>;
+  return (
+    <div className="App">
+      Hello world from Netlify after change to check automatic deploy!{" "}
+    </div>
+  );
 }
 
 export default App;
